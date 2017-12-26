45cc7923961b2c9a0438e2c67e169b02437071c3
Jonathan Yee <jonathan.yee@ucalgary.ca>
2017-12-26 15:50:18 -0700

    gui building initialized

--- src/main_w.java
+++ src/main_w.java
@@ -1,4 +1,7 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JSplitPane;
 /**
  * 
  * @author Jonathan
@@ -8,9 +11,17 @@ public class main_w extends JFrame{
 	
 	public main_w(){
 		
+		JSplitPane splitPane = new JSplitPane();
+		getContentPane().add(splitPane, BorderLayout.CENTER);
+		
 	}
 	public static void main(String[] args) {
-
+		main_w window = new main_w();
+		window.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		//window.pack();
+		//window.setTitle("Easy Folder Name'er");
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
 	}
 
 }
