d22486e0b5b7ca530473dd7167f92a7a8ce3a9d7
Jonathan Yee <jonathan.yee@ucalgary.ca>
2017-12-26 15:35:36 -0700

    newest main java file

---
+++ src/main_w.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+/**
+ * 
+ * @author Jonathan
+ *
+ */
+public class main_w extends JFrame{
+	
+	public main_w(){
+		
+	}
+	public static void main(String[] args) {
+
+	}
+
+}
